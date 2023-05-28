169f0623af70da9b5cf16e05d7784175924fc0a2
CyberButton <87766623307Kk>
2023-05-28 20:43:42 +0600

    Initial commit

---
+++ src/test/java/com/nurtore/nurtorecurrency/NurtorecurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurtore.nurtorecurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurtorecurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
